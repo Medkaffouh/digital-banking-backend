3e11ff1f1c95dabad37f21defda47db7bea8993f
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-17 12:35:31 +0100

    Initial commit

---
+++ src/main/java/com/med/digitalbankingbackend/DigitalBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.med.digitalbankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/med/digitalbankingbackend/DigitalBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.digitalbankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
