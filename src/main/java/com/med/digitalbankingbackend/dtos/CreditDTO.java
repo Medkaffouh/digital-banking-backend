52b9f1fbb450235cf28abd6c7f886bbdea132f2b
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-19 23:48:52 +0100

    creation debit et credit DTO

---
+++ src/main/java/com/med/digitalbankingbackend/dtos/CreditDTO.java
@@ -0,0 +1,7 @@
+package com.med.digitalbankingbackend.dtos;
+
+public class CreditDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
---
+++ src/main/java/com/med/digitalbankingbackend/dtos/DebitDTO.java
@@ -0,0 +1,7 @@
+package com.med.digitalbankingbackend.dtos;
+
+public class DebitDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
